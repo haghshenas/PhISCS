0c9af2d3b8f85eee5d780623d01bce14fbb50cd6
Simone Ciccolella <s.ciccolella@campus.unimib.it>
2017-10-18 13:14:51 -0400

    ILP updated accordingly to README

--- src/ilp/big_brother.py
+++ src/ilp/big_brother.py
@@ -3,84 +3,72 @@ from gurobipy import *
 import numpy as np
 from datetime import datetime
 import argparse
+import os
 
 # ======== INFO
 '''
 This version of the ILP allows EVERITHING.
 
-Should be used for test purposes.
+TODO: VAF
 '''
 
 # ======== COMMAND LINE THINGS
 parser = argparse.ArgumentParser(description='big_brother', add_help=True)
-parser.add_argument('-i', '--id', required = True,
-					type = int,
-					help = 'Simulation ID')
-parser.add_argument('-fn', '--falseneg', required = True,
-					type = float,
-					help = 'False negative rate')
-parser.add_argument('-fp', '--falsepos', required = True,
-					type = float,
-					help = 'False positive rate')
-parser.add_argument('-c', '--cells', required = True,
-					type = int,
-					help = 'Number of cells')
-parser.add_argument('-k', '--kmax', default = 0,
-					type = int,
-					help = 'Max number column removals [Default is 0]')
-parser.add_argument('-d', '--dir', required = True,
+# Required:
+parser.add_argument('-f', '--file', required = True,
 					type = str,
-					help = 'Simulation data directory (should contain ground/ and noisy/')
-parser.add_argument('-t', '--threads', default = 4,
+					help = 'Input matrix file')
+parser.add_argument('-n', '--fnWeight', required = True,
 					type = int,
-					help = 'Number of threads allowed [Default is 4]')
-parser.add_argument('-u', '--custom', nargs=2, required = True,
-				   type = int,
-				   help = 'Custom weights for FN and FP')
+					help = 'Weight for false negative')
+parser.add_argument('-p', '--fpWeight', required = True,
+					type = int,
+					help = 'Weight for false negative')
+parser.add_argument('-o', '--outDir', required = True,
+					type = str,
+					help = 'Output directory')
+
+# Optional:
+parser.add_argument('-g', '--ground', default = None,
+					type = str,
+					help = 'Ground truth matrix [""]')
+parser.add_argument('-m', '--maxMut', default = 0,
+					type = int,
+					help = 'Max number mutations to be eliminated [0]')
+
+parser.add_argument('-t', '--threads', default = 1,
+					type = int,
+					help = 'Number of threads [Default is 1]')
 
 args = parser.parse_args()
 
-# ======== INPUT PROCESSING FOR NOISY DATA:
+# ======== INPUT PROCESSING FOR DATA:
+start_model = datetime.now()
+DELIMITER = '\t'
 
-sim_id = args.id
-fn_rate = args.falseneg
-arg_cell = args.cells
-fp_rate = args.falsepos
+verbose = False
 
-fp_str = ''
-if fp_rate != float(0):
-	fp_str = '-fp_{0}'.format(str(fp_rate))
+inp = np.genfromtxt(args.file, skip_header = 1, delimiter = DELIMITER)
 
-
-folder = args.dir
-sim_file = 'noisyFile-simID_{0}-n_{1}-m_50-fn_{2}{3}.txt'.format(
-	str(sim_id), str(arg_cell), str(fn_rate), fp_str)
-
-inp = np.genfromtxt(folder + '/noisy/' + sim_file, skip_header=1, delimiter='\t')
+with open(args.file, 'r') as fin:
+	mutation_names = fin.readline().strip().split(DELIMITER)[1 :]
+	cell_names = [ x.strip().split(DELIMITER)[0] for x in fin ]
 
 matrix_input = np.delete(inp, 0, 1)
 
-with open(folder + '/noisy/' + sim_file, 'r') as fin:
-	mutation_names = fin.readline().strip().split('\t')[1 :]
-	cell_names = [ x.strip().split('\t')[0] for x in fin ]
-
-print(mutation_names)
-print(cell_names)
-
 # =========== GENERAL INITIALIZATION
-
 cells = matrix_input.shape[0]
 mutations = matrix_input.shape[1]
 
-# k_max = matrix_input.shape[1] * 0.1
-k_max = args.kmax
+k_max = args.maxMut
 
-fn_weight, fp_weight = args.custom
+fn_weight = args.fnWeight
+fp_weight = args.fpWeight
 
 # =========== VARIABLES
 model = Model('Reduced ILP')
 model.Params.Threads = args.threads
-# model.Params.timeLimit = 300.0
+model.Params.LogFile = ""
 
 print('Generating variables...')
 
@@ -204,21 +192,21 @@ while p < mutations:
 		q += 1
 	p += 1
 
+time_to_model = datetime.now() - start_model
 # ====== OPTIMIZE
-
 start_optimize = datetime.now()
 model.optimize()
 
 # ====== POST OPTIMIZATION
+time_to_opt = datetime.now() - start_optimize
 
-print('-' * 20)
-print('Time')
-time_to_end = datetime.now() - start_optimize
-print(time_to_end)
+if verbose:
+	print('-' * 20)
+	print('Time')
+	print(time_to_end)
+	print('-' * 20)
+	print('Flipped 0 -> 1')
 
-
-print('-' * 20)
-print('Flipped 0 -> 1')
 flip0_matrix = []
 flip0_sol_tot = 0
 
@@ -233,14 +221,16 @@ while c < cells:
 		else:
 			row.append(0)
 		m += 1
-	print(' '.join([str(x) for x in row]))
+	if verbose:
+		print(' '.join([str(x) for x in row]))
 	flip0_matrix.append(row)
 	c += 1
 
 flip0_matrix = np.array(flip0_matrix)
 
-print('-' * 20)
-print('Flipped 1 -> 0')
+if verbose:
+	print('-' * 20)
+	print('Flipped 1 -> 0')
 flip1_matrix = []
 flip1_sol_tot = 0
 c = 0
@@ -254,18 +244,38 @@ while c < cells:
 		else:
 			row.append(0)
 		m += 1
-	print(' '.join([str(x) for x in row]))
+	if verbose:
+		print(' '.join([str(x) for x in row]))
 	flip1_matrix.append(row)
 	c += 1
 
 flip1_matrix = np.array(flip1_matrix)
 
+filename = os.path.splitext(os.path.basename(args.file))[0]
+outfile = os.path.join(args.outDir, filename)
+file_out = open('{0}.output'.format(outfile), 'w+')
 
-file_out = open('out/sol_{0}'.format(sim_file), 'w+')
-log = open('out/log_{0}'.format(sim_file), 'w+')
+# --- Solution info
+removed_cols = []
+removed_mutation_names = []
+solution_mutation_names = []
+m = 0
+while m < mutations:
+	value = int(K[m].X)
+	removed_cols.append(value)
+	if value == 0:
+		solution_mutation_names.append(mutation_names[m])
+	else:
+		removed_mutation_names.append(mutation_names[m])
+	m += 1
 
-print('-' * 20)
-print('Result')
+file_out.write('cellID/mutID')
+file_out.write('\t'.join(solution_mutation_names))
+file_out.write('\n')
+
+if verbose:
+	print('-' * 20)
+	print('Result')
 sol_matrix = []
 c = 0
 while c < cells:
@@ -280,8 +290,10 @@ while c < cells:
 			else:
 				row.append(int(X[c, m].X))
 		m += 1
-	print(' '.join([str(x) for x in row]))
-	file_out.write(','.join([str(x) for x in row]))
+	if verbose:
+		print(' '.join([str(x) for x in row]))
+	file_out.write('%s\t' % cell_names[c])
+	file_out.write('\t'.join([str(x) for x in row]))
 	file_out.write('\n')
 
 	sol_matrix.append(row)
@@ -291,116 +303,108 @@ sol_matrix = np.array(sol_matrix)
 
 file_out.close()
 
-# --- Input info
-log.write('SIM_ID: {0}\n'.format(str(sim_id)))
-log.write('NUM_MUTATIONS(COLUMNS): {0}\n'.format(str(mutations)))
-log.write('NUM_ROWS(CELLS): {0}\n'.format(str(cells)))
-log.write('RUNNING_TIME_SECONDS: {0}\n'.format(str(time_to_end.total_seconds())))
-log.write('FALSE_NEGATIVE_RATE: {0}\n'.format(str(fn_rate)))
+if args.ground:
+	log = open('{0}.log'.format(outfile), 'w+')
+	# --- Input info
+	log.write('FILE_NAME: {0}\n'.format(str(os.path.basename(args.file))))
+	log.write('NUM_ROWS(CELLS): {0}\n'.format(str(cells)))
+	log.write('NUM_MUTATIONS(COLUMNS): {0}\n'.format(str(mutations)))
 
-# -- Ground info
-ground_file = 'simID_{0}-n_{1}-m_50.txt'.format(
-	str(sim_id), str(arg_cell))
-ground_matrix = np.genfromtxt(folder + '/ground/' + ground_file, skip_header=1, usecols=range(1, 51))
+	# -- Ground info
 
-flips_in_noisy = 0
-flips01_in_noisy = 0
-flips10_in_noisy = 0
-flips01_matrix = np.zeros((cells, mutations))
-flips10_matrix = np.zeros((cells, mutations))
-for c in range(cells):
-	for m in range(mutations):
-		if ground_matrix[c,m] == 0 and matrix_input[c,m] == 1:
-			flips01_in_noisy += 1
-			flips_in_noisy += 1
-			flips01_matrix[c, m] = 1
+	# ground_file = 'simID_{0}-n_{1}-m_50.txt'.format(
+	# 	str(sim_id), str(arg_cell))
+	# ground_matrix = np.genfromtxt(folder + '/ground/' + ground_file, skip_header=1, usecols=range(1, 51))
+	#
+	# flips_in_noisy = 0
+	# flips01_in_noisy = 0
+	# flips10_in_noisy = 0
+	# flips01_matrix = np.zeros((cells, mutations))
+	# flips10_matrix = np.zeros((cells, mutations))
+	# for c in range(cells):
+	# 	for m in range(mutations):
+	# 		if ground_matrix[c,m] == 0 and matrix_input[c,m] == 1:
+	# 			flips01_in_noisy += 1
+	# 			flips_in_noisy += 1
+	# 			flips01_matrix[c, m] = 1
+	#
+	# 		if ground_matrix[c,m] == 1 and matrix_input[c,m] == 0:
+	# 			flips10_in_noisy += 1
+	# 			flips_in_noisy += 1
+	# 			flips10_matrix[c, m] = 1
+	#
+	# log.write('TOTAL_FLIPS_INTRODUCED_BY_NOISY_COMPARED_TO_GROUND: {0}\n'.format(
+	# 	str(flips_in_noisy)))
+	# log.write('0_1_FLIPS_INTRODUCED_BY_NOISY_COMPARED_TO_GROUND: {0}\n'.format(
+	# 	str(flips01_in_noisy)))
+	# log.write('1_0_FLIPS_INTRODUCED_BY_NOISY_COMPARED_TO_GROUND: {0}\n'.format(
+	# 	str(flips10_in_noisy)))
 
-		if ground_matrix[c,m] == 1 and matrix_input[c,m] == 0:
-			flips10_in_noisy += 1
-			flips_in_noisy += 1
-			flips10_matrix[c, m] = 1
+	log.write('TOTAL_FLIPS_REPORTED_BY_SOLUTION_COMPARED_TO_NOISY: {0}\n'.format(
+		str(flip0_sol_tot + flip1_sol_tot)))
+	log.write('0_1_FLIPS_REPORTED_BY_SOLUTION_COMPARED_TO_NOISY: {0}\n'.format(
+		str(flip0_sol_tot)))
+	log.write('1_0_FLIPS_REPORTED_BY_SOLUTION_COMPARED_TO_NOISY: {0}\n'.format(
+		str(flip1_sol_tot)))
+	log.write('COLUMNS REMOVED: {0}\n'. format(
+		sum(removed_cols)))
+	log.write('MUTATIONS REMOVED: {0}\n'.format(
+		','.join(removed_mutation_names)))
 
-log.write('TOTAL_FLIPS_INTRODUCED_BY_NOISY_COMPARED_TO_GROUND: {0}\n'.format(
-	str(flips_in_noisy)))
-log.write('0_1_FLIPS_INTRODUCED_BY_NOISY_COMPARED_TO_GROUND: {0}\n'.format(
-	str(flips01_in_noisy)))
-log.write('1_0_FLIPS_INTRODUCED_BY_NOISY_COMPARED_TO_GROUND: {0}\n'.format(
-	str(flips10_in_noisy)))
+	# --- Overlap info
 
-# --- Solution info
-removed_cols = []
-removed_mutation_names = []
-solution_mutation_names = []
-m = 0
-while m < mutations:
-	value = int(K[m].X)
-	removed_cols.append(value)
-	if value == 1:
-		removed_mutation_names.append(str(m))
-	m += 1
+	# overlap010 = 0
+	# overlap101 = 0
+	# overlap_total = 0
+	#
+	# for c in range(cells):
+	# 	for m in range(mutations):
+	# 		if flips01_matrix[c, m] == 1 and flip1_matrix[c, m] == 1:
+	# 			overlap010 += 1
+	# 			overlap_total += 1
+	# 		elif flips10_matrix[c, m] and flip0_matrix[c, m] == 1:
+	# 			overlap101 += 1
+	# 			overlap_total += 1
+	# 		elif flips01_matrix[c, m] == 1 and flip1_matrix[c, m] == 1:
+	# 			overlap_total += 1
+	# 		elif flips10_matrix[c, m] == 1 and flip0_matrix[c, m] == 1:
+	# 			overlap_total += 1
+	#
+	# log.write('TOTAL_OVERLAP_NOISE_FLIPS_SOLUTION_FLIPS: {0}\n'.format(
+	# 	str(overlap_total)))
+	# log.write('0_1_0_OVERLAP_NOISE_FLIPS_SOLUTION_FLIPS: {0}\n'.format(
+	# 	str(overlap010)))
+	# log.write('1_0_1_OVERLAP_NOISE_FLIPS_SOLUTION_FLIPS: {0}\n'.format(
+	# 	str(overlap101)))
 
-log.write('TOTAL_FLIPS_REPORTED_BY_SOLUTION_COMPARED_TO_NOISY: {0}\n'.format(
-	str(flip0_sol_tot + flip1_sol_tot)))
-log.write('0_1_FLIPS_REPORTED_BY_SOLUTION_COMPARED_TO_NOISY: {0}\n'.format(
-	str(flip0_sol_tot)))
-log.write('1_0_FLIPS_REPORTED_BY_SOLUTION_COMPARED_TO_NOISY: {0}\n'.format(
-	str(flip1_sol_tot)))
-log.write('COLUMNS REMOVED: {0}\n'. format(
-	sum(removed_cols)))
-log.write('MUTATIONS REMOVED: {}\n'.format(
-	','.join(removed_mutation_names)))
+	# --- DOUBLE-CHECK PP
+	conflict_free = True
+	for p in range(sol_matrix.shape[1]):
+		for q in range(p + 1, sol_matrix.shape[1]):
+			oneone = False
+			zeroone = False
+			onezero = False
+			for r in range(sol_matrix.shape[0]):
+				if sol_matrix[r][p] == 1 and sol_matrix[r][q] == 1:
+					oneone = True
+				if sol_matrix[r][p] == 0 and sol_matrix[r][q] == 1:
+					zeroone = True
+				if sol_matrix[r][p] == 1 and sol_matrix[r][q] == 0:
+					onezero = True
 
-# --- Overlap info
+			if oneone and zeroone and onezero:
+				conflict_free = False
+				if verbose:
+					print('Conflict in columns (%d, %d)' % (p, q))
 
-overlap010 = 0
-overlap101 = 0
-overlap_total = 0
+	log.write('CONFLICT_FREE: {0}\n'.format(str(conflict_free)))
+	log.write('NUM_THREADS: {0}\n'.format(str(args.threads)))
+	log.write('MODEL_BUILD_TIME_SECONDS: {0}\n'.format(str(time_to_model.total_seconds())))
+	log.write('RUNNING_TIME_SECONDS: {0}\n'.format(str(time_to_opt.total_seconds())))
 
-for c in range(cells):
-	for m in range(mutations):
-		if flips01_matrix[c, m] == 1 and flip1_matrix[c, m] == 1:
-			overlap010 += 1
-			overlap_total += 1
-		elif flips10_matrix[c, m] and flip0_matrix[c, m] == 1:
-			overlap101 += 1
-			overlap_total += 1
-		elif flips01_matrix[c, m] == 1 and flip1_matrix[c, m] == 1:
-			overlap_total += 1
-		elif flips10_matrix[c, m] == 1 and flip0_matrix[c, m] == 1:
-			overlap_total += 1
+	log.close()
 
-log.write('TOTAL_OVERLAP_NOISE_FLIPS_SOLUTION_FLIPS: {0}\n'.format(
-	str(overlap_total)))
-log.write('0_1_0_OVERLAP_NOISE_FLIPS_SOLUTION_FLIPS: {0}\n'.format(
-	str(overlap010)))
-log.write('1_0_1_OVERLAP_NOISE_FLIPS_SOLUTION_FLIPS: {0}\n'.format(
-	str(overlap101)))
-
-# --- DOUBLE-CHECK PP
-conflict_free = True
-for p in range(sol_matrix.shape[1]):
-	for q in range(p + 1, sol_matrix.shape[1]):
-		oneone = False
-		zeroone = False
-		onezero = False
-		for r in range(sol_matrix.shape[0]):
-			if sol_matrix[r][p] == 1 and sol_matrix[r][q] == 1:
-				oneone = True
-			if sol_matrix[r][p] == 0 and sol_matrix[r][q] == 1:
-				zeroone = True
-			if sol_matrix[r][p] == 1 and sol_matrix[r][q] == 0:
-				onezero = True
-
-		if oneone and zeroone and onezero:
-			conflict_free = False
-			print('Conflict in columns (%d, %d)' % (p, q))
-
-log.write('CONFLICT_FREE: {0}\n'.format(str(conflict_free)))
-log.write('NUM_THREADS: {0}\n'.format(str(4)))
-log.write('CPU_CLOCK: {0}\n'.format('2.7 GHz'))
-
-log.close()
-
-# Tree construction
-from tree import *
-write_tree_comp(sol_matrix, mutation_names, '%s' % sim_file)
+else:
+	# Tree construction
+	from tree import *
+	write_tree(sol_matrix, mutation_names, outfile)
