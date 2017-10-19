b02fc7ffd5deea087ab99069ceac40446e627f19
Farid Rashidi <Farid@149-160-223-129.dhcp-bl.indiana.edu>
2017-10-19 11:41:10 -0400

    fixed bug of generate noisy with na

--- src/generate_noisy/noisy.py
+++ src/generate_noisy/noisy.py
@@ -11,7 +11,7 @@ def read_data(file):
 
 def write_noisy_both(outresult, file, percfn, percfp, percna, perck):
 	file = file.replace('ground','noisy')
-	file = file.replace('.txt','')
+	file = file.replace('-k_0.noisyTruthMatrix','')
 	f = file+'-fn_'+percfn+'-fp_'+percfp+'-na_'+percna+'-k_'+perck+'.noisyMatrix'
 	df = pd.DataFrame(outresult)
 	df = df.add_prefix('mut')
@@ -26,6 +26,7 @@ def introduce_false_both(data, n, m, percfn, percfp, percna):
 	indexFN = []
 	indexFP = []
 	indexNA = []
+	changedBefore = []
 	for i in range(n):
 		for j in range(m):
 			countOneZero = countOneZero + 1
@@ -42,15 +43,33 @@ def introduce_false_both(data, n, m, percfn, percfp, percna):
 	random.shuffle(indexFN)
 	random.shuffle(indexFP)
 	random.shuffle(indexNA)
-	for i in range(int(falsenegative)):
-		[a,b] = indexFN[i]
-		data[a][b] = 0
-	for i in range(int(falsepositive)):
-		[a,b] = indexFP[i]
-		data[a][b] = 1
 	for i in range(int(nas)):
-		[a,b] = indexFP[i]
+		[a,b] = indexNA[i]
+		#print "NA: ", [a,b]
+		changedBefore.append([a,b])
 		data[a][b] = 2
+	counter = int(falsenegative)-1
+	p = 0
+	while counter>=0:
+		if indexFN[p] in changedBefore:
+			p = p+1
+		else:
+			p = p+1
+			[a,b] = indexFN[p]
+			#print "FN: ", [a,b]
+			data[a][b] = 0
+			counter = counter - 1
+	counter = int(falsepositive)-1
+	p = 0
+	while counter>=0:
+		if indexFP[p] in changedBefore:
+			p = p+1
+		else:
+			p = p+1
+			[a,b] = indexFP[p]
+			#print "FP: ",[a,b]
+			data[a][b] = 1
+			counter = counter - 1
 	return data
 
 
