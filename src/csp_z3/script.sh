244371c83b596a9dd2be2b094a8e4ea0242c36e5
Farid Rashidi <farid.rsh@gmail.com>
2017-10-19 13:02:19 -0400

    fixed its bug

--- src/csp_z3/script.sh
+++ src/csp_z3/script.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-directory="../../data/simulated/19oct/noisy"
+directory="../../data/simulated/19oct"
 o="./result"
 z3="../../../z3-master/build/z3"
 
+n="100"
+m="50"
 percentsFN=(0.15 0.25)
 percentsFP=(0.0001)
 percentsNA=(0.15)
-percentsK=(0)
+k="0"
 
-files="$(find $directory -type f -name '*.noisyMatrix' | sort)"
-
-for f in ${files}
+for id in {1..30}
 do
-	echo "File: "$f
 	for fn in ${percentsFN[@]}
 	do
-		echo "FN: "$fn
 		for fp in ${percentsFP[@]}
 		do
-			echo "FP: "$fp
 			for na in ${percentsNA[@]}
 			do	
 				fpW=$(echo "$(echo $fn/$fp|bc -l)/1"|bc)
-				g=$(echo $f | sed "s/noisy/ground/g")
-				g=$(echo $g | sed "s/-fn_${fn}//g")
-				g=$(echo $g | sed "s/-fp_${fp}//g")
-				g=$(echo $g | sed "s/-na_${na}//g")
-				g=$(echo $g | sed "s/groundMatrix/groundTruthMatrix/g")
+				f=$directory"/noisy/simID_"${id}"-n_"${n}"-m_"${m}"-fn_"${fn}"-fp_"${fp}"-na_"${na}"-k_"${k}".noisyMatrix"
+				g=$directory"/ground/simID_"${id}"-n_"${n}"-m_"${m}"-k_"${k}".groundTruthMatrix"
+				echo "Noisy: "$f
+				echo "Ground: "$g
 				python main.py -f $f -n 1 -p $fpW -o $o -g $g -z3path $z3
 			done
 			
