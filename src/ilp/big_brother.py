9472a3c5b44fea5ddc772781595c266eec4c008f
Simone Ciccolella <s.ciccolella@campus.unimib.it>
2017-10-26 15:12:20 -0400

    VAF constraints linearized

--- src/ilp/big_brother.py
+++ src/ilp/big_brother.py
@@ -3,44 +3,43 @@ from gurobipy import *
 import numpy as np
 from datetime import datetime
 import argparse
-import os, errno
+import os
+import errno
 
 # ======== INFO
 '''
 This version of the ILP allows EVERITHING.
-
-TODO: VAF
 '''
 
 # ======== COMMAND LINE THINGS
 parser = argparse.ArgumentParser(description='big_brother', add_help=True)
 # Required:
-parser.add_argument('-f', '--file', required = True,
-					type = str,
-					help = 'Input matrix file')
-parser.add_argument('-n', '--fnWeight', required = True,
-					type = int,
-					help = 'Weight for false negative')
-parser.add_argument('-p', '--fpWeight', required = True,
-					type = int,
-					help = 'Weight for false negative')
-parser.add_argument('-o', '--outDir', required = True,
-					type = str,
-					help = 'Output directory')
+parser.add_argument('-f', '--file', required=True,
+                    type=str,
+                    help='Input matrix file')
+parser.add_argument('-n', '--fnWeight', required=True,
+                    type=int,
+                    help='Weight for false negative')
+parser.add_argument('-p', '--fpWeight', required=True,
+                    type=int,
+                    help='Weight for false negative')
+parser.add_argument('-o', '--outDir', required=True,
+                    type=str,
+                    help='Output directory')
 
 # Optional:
-parser.add_argument('-m', '--maxMut', default = 0,
-					type = int,
-					help = 'Max number mutations to be eliminated [0]')
-parser.add_argument('-t', '--threads', default = 1,
-					type = int,
-					help = 'Number of threads [Default is 1]')
-parser.add_argument('-b', '--bulk', default = None,
-					type = str,
-					help = 'Bulk sequencing file [""]')
-parser.add_argument('-e', '--delta', default = 0.01,
-					type = float,
-					help = 'Delta in VAF [0.01]')
+parser.add_argument('-m', '--maxMut', default=0,
+                    type=int,
+                    help='Max number mutations to be eliminated [0]')
+parser.add_argument('-t', '--threads', default=1,
+                    type=int,
+                    help='Number of threads [Default is 1]')
+parser.add_argument('-b', '--bulk', default=None,
+                    type=str,
+                    help='Bulk sequencing file [""]')
+parser.add_argument('-e', '--delta', default=0.01,
+                    type=float,
+                    help='Delta in VAF [0.01]')
 
 args = parser.parse_args()
 
@@ -49,13 +48,13 @@ start_model = datetime.now()
 DELIMITER = '\t'
 
 verbose = False
-tree = False
+tree = True
 
-inp = np.genfromtxt(args.file, skip_header = 1, delimiter = DELIMITER)
+inp = np.genfromtxt(args.file, skip_header=1, delimiter=DELIMITER)
 
 with open(args.file, 'r') as fin:
-	mutation_names = fin.readline().strip().split(DELIMITER)[1 :]
-	cell_names = [ x.strip().split(DELIMITER)[0] for x in fin ]
+    mutation_names = fin.readline().strip().split(DELIMITER)[1:]
+    cell_names = [x.strip().split(DELIMITER)[0] for x in fin]
 
 matrix_input = np.delete(inp, 0, 1)
 
@@ -70,15 +69,15 @@ fp_weight = args.fpWeight
 
 using_bulk = False
 if args.bulk:
-	delta = args.delta
-	using_bulk = True
-	bulk_mutations = []
-	with open(args.bulk, 'r') as bulkfile:
-		bulkfile.readline()
-		for line in bulkfile:
-			values = line.split('\t')
-			vaf = float(values[3]) / float(values[4])
-			bulk_mutations.append(vaf)
+    delta = args.delta
+    using_bulk = True
+    bulk_mutations = []
+    with open(args.bulk, 'r') as bulkfile:
+        bulkfile.readline()
+        for line in bulkfile:
+            values = line.split('\t')
+            vaf = float(values[3]) / float(values[4])
+            bulk_mutations.append(vaf)
 
 # =========== VARIABLES
 model = Model('Reduced ILP')
@@ -87,88 +86,83 @@ model.Params.LogFile = ""
 
 print('Generating variables...')
 
-# --- Flips 0->1 [False Negative / Allele dropout]
+# --- Flips/Completion
 F0 = {}
-c = 0
-while c < cells:
-	m = 0
-	while m < mutations:
-		if matrix_input[c][m] == 0:
-			F0[c, m] = model.addVar(vtype = GRB.BINARY,
-							   obj = fn_weight,
-							   name='B({0},{1})'.format(c, m))
-		else:
-			F0[c,m] = 0
-		m += 1
-	c += 1
-
-# --- Flips 1->0 [False Positive]
 F1 = {}
-c = 0
-while c < cells:
-	m = 0
-	while m < mutations:
-		if matrix_input[c][m] == 1:
-			F1[c, m] = model.addVar(vtype=GRB.BINARY,
-							   obj= fp_weight,
-							   name='B({0},{1})'.format(c, m))
-		else:
-			F1[c,m] = 0
-		m += 1
-	c += 1
-
-# --- Missing values
 X = {}
 c = 0
 while c < cells:
-	m = 0
-	while m < mutations:
-		if matrix_input[c][m] == 2:
-			X[c, m] = model.addVar(vtype=GRB.BINARY,
-							   obj= 0,
-							   name='B({0},{1})'.format(c, m))
-		else:
-			X[c,m] = 0
-		m += 1
-	c += 1
+    m = 0
+    while m < mutations:
+        # 0->1 [False Negative / Allele dropout]
+        if matrix_input[c][m] == 0:
+            F0[c, m] = model.addVar(vtype=GRB.BINARY,
+                                    obj=fn_weight,
+                                    name='B({0},{1})'.format(c, m))
+        else:
+            F0[c, m] = 0
 
+        # 1->0 [False Positive]
+        if matrix_input[c][m] == 1:
+            F1[c, m] = model.addVar(vtype=GRB.BINARY,
+                                    obj=fp_weight,
+                                    name='B({0},{1})'.format(c, m))
+        else:
+            F1[c, m] = 0
+        # Missing values
+        if matrix_input[c][m] == 2:
+            X[c, m] = model.addVar(vtype=GRB.BINARY,
+                                   obj=0,
+                                   name='B({0},{1})'.format(c, m))
+        else:
+            X[c, m] = 0
+
+        m += 1
+    c += 1
 
 # --- Conflict counter
 B = {}
 p = 0
 while p < mutations:
-	q = p + 1
-	while q < mutations:
-		B[p, q, 1, 1] = model.addVar(vtype=GRB.BINARY, obj=0,
-				name='B[{0},{1},1,1]'.format(p, q))
-		B[p, q, 1, 0] = model.addVar(vtype=GRB.BINARY, obj=0,
-				name='B[{0},{1},1,0]'.format(p, q))
-		B[p, q, 0, 1] = model.addVar(vtype=GRB.BINARY, obj=0,
-				name='B[{0},{1},0,1]'.format(p, q))
-		q += 1
-	p += 1
+    q = 0
+    while q < mutations:
+        B[p, q, 1, 1] = model.addVar(vtype=GRB.BINARY, obj=0,
+                                     name='B[{0},{1},1,1]'.format(p, q))
+        B[p, q, 1, 0] = model.addVar(vtype=GRB.BINARY, obj=0,
+                                     name='B[{0},{1},1,0]'.format(p, q))
+        B[p, q, 0, 1] = model.addVar(vtype=GRB.BINARY, obj=0,
+                                     name='B[{0},{1},0,1]'.format(p, q))
+        q += 1
+    p += 1
 
 
 # --- Column delition
 K = {}
 m = 0
 while m < mutations:
-	K[m] = model.addVar(vtype=GRB.BINARY,
-						obj=0,
-						name='K[{0}]'.format(m))
-	m += 1
+    K[m] = model.addVar(vtype=GRB.BINARY,
+                        obj=0,
+                        name='K[{0}]'.format(m))
+    m += 1
 
 if using_bulk:
-	A = {}
-	p = 0
-	while p < mutations:
-		q = 0
-		while q < mutations:
-			A[p, q] = model.addVar(vtype=GRB.BINARY, obj=0,
-					name='A[{0},{1}]'.format(p, q))
-			q += 1
-		p += 1
+    A = {}
+    C1 = {}
+    C2 = {}
+    p = 0
+    while p < mutations:
+        q = 0
+        while q < mutations:
+            A[p, q] = model.addVar(vtype=GRB.BINARY, obj=0,
+                                   name='A[{0},{1}]'.format(p, q))
+            r = 0
+            while r < cells:
+                C1[r,p,q] = model.addVar(vtype=GRB.BINARY, obj=0)
+                C2[r,p,q] = model.addVar(vtype=GRB.BINARY, obj=0)
+                r += 1
 
+            q += 1
+        p += 1
 
 model.modelSense = GRB.MINIMIZE
 model.update()
@@ -182,66 +176,79 @@ model.addConstr(quicksum(K[m] for m in range(mutations)) <= k_max)
 # --- B(p, q, a, b) variables
 c = 0
 while c < cells:
-	p = 0
-	while p < mutations:
-		q = p + 1
-		while q < mutations:
-			model.addConstr(
-				(matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p]) +
-				(matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q]) -
-				B[p, q, 1, 1] <= 1,
-				'B[{0},{1},1,1]_{2}'.format(p, q, c))
-			model.addConstr(
-				- (matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p]) +
-				(matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q]) -
-				B[p, q, 0, 1] <= 0,
-				'B[{0},{1},0,1]_{2}'.format(p, q, c))
-			model.addConstr(
-				(matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p]) -
-				(matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q]) -
-				B[p, q, 1, 0] <= 0,
-				'B[{0},{1},1,0]_{2}'.format(p, q, c))
-			q += 1
-		p += 1
-	c += 1
+    p = 0
+    while p < mutations:
+        q = 0
+        while q < mutations:
+            model.addConstr(
+                (matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p]) +
+                (matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q]) -
+                B[p, q, 1, 1] <= 1,
+                'B[{0},{1},1,1]_{2}'.format(p, q, c))
+            model.addConstr(
+                - (matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p]) +
+                (matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q]) -
+                B[p, q, 0, 1] <= 0,
+                'B[{0},{1},0,1]_{2}'.format(p, q, c))
+            model.addConstr(
+                (matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p]) -
+                (matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q]) -
+                B[p, q, 1, 0] <= 0,
+                'B[{0},{1},1,0]_{2}'.format(p, q, c))
+            q += 1
+        p += 1
+    c += 1
 
 # --- No conflict between columns
 p = 0
 while p < mutations:
-	q = p + 1
-	while q < mutations:
-		model.addConstr(
-			B[p, q, 0, 1] + B[p, q, 1, 0] + B[p, q, 1, 1]
-			- K[p] - K[q]
-			<= 2,
-			'Conf[{0},{1}]'.format(p, q))
-		q += 1
-	p += 1
+    q = 0
+    while q < mutations:
+        model.addConstr(
+            B[p, q, 0, 1] + B[p, q, 1, 0] + B[p, q, 1, 1]
+            - K[p] - K[q]
+            <= 2,
+            'Conf[{0},{1}]'.format(p, q))
+        q += 1
+    p += 1
 
 # --- Constraint for VAFs
 if using_bulk:
-	p = 0
-	while p < mutations:
-		q = 0
-		while q < mutations:
-			c = 0
-			while c < cells:
-				model.addConstr(A[p, q] * (matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p]) >= A[p, q] *(matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q]) )
-				c += 1
+    p = 0
+    while p < mutations:
+        q = 0
+        while q < mutations:
+            c = 0
+            while c < cells:
+                model.addConstr(C1[c,p,q] <= A[p, q])
+                model.addConstr(C1[c,p,q] <= matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p])
+                model.addConstr(C1[c,p,q] >= matrix_input[c, p] % 2 + F0[c, p] - F1[c, p] + X[c, p] + A[p, q] - 1)
 
-			model.addConstr(A[p, q] + A[q, p] <= 1)
-			model.addConstr(A[p, q] <= 1 - K[p])
-			model.addConstr(A[p, q] <= 1 - K[q])
+                model.addConstr(C2[c,p,q] <= A[p, q])
+                model.addConstr(C2[c,p,q] <= matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q])
+                model.addConstr(C2[c,p,q] >= matrix_input[c, q] % 2 + F0[c, q] - F1[c, q] + X[c, q] + A[p, q] - 1)
 
-			model.addConstr(A[p, q] * bulk_mutations[p]  * (1 + delta) >= A[p, q] * bulk_mutations[q])
+                model.addConstr(C1[c,p,q] >= C2[c,p,q])
+                c += 1
 
-			r = 0
-			while r < mutations:
-				model.addConstr(bulk_mutations[p] * (1 + delta) >= bulk_mutations[q] * (A[p, q] - A[r, q] - A[q, r]) + bulk_mutations[r] * (A[p, r] - A[r,q] - A[q, r]))
-				r += 1
+            model.addConstr(A[p, q] + A[q, p] <= 1)
+            model.addConstr(A[p, q] <= 1 - K[p])
+            model.addConstr(A[p, q] <= 1 - K[q])
 
-			q += 1
-		p += 1
+            model.addConstr(A[q, p] <= 1 - K[p])
+            model.addConstr(A[q, p] <= 1 - K[q])
+
+            model.addConstr(A[p, q] * bulk_mutations[p] * (1 + delta)
+                            >= A[p, q] * bulk_mutations[q])
+
+            r = 0
+            while r < mutations:
+                model.addConstr(bulk_mutations[p] * (1 + delta) >= bulk_mutations[q] * (
+                    A[p, q] - A[r, q] - A[q, r]) + bulk_mutations[r] * (A[p, r] - A[r, q] - A[q, r]))
+                r += 1
+
+            q += 1
+        p += 1
 
 time_to_model = datetime.now() - start_model
 # ====== OPTIMIZE
@@ -250,67 +257,73 @@ start_optimize = datetime.now()
 model.optimize()
 
 # ====== POST OPTIMIZATION
+if model.status == GRB.Status.INFEASIBLE:
+    print('The model is unfeasible.')
+    exit(0)
+
+
+
 time_to_opt = datetime.now() - start_optimize
 time_to_run = datetime.now() - start_model
 
 if verbose:
-	print('-' * 20)
-	print('Time')
-	print(time_to_end)
-	print('-' * 20)
-	print('Flipped 0 -> 1')
+    print('-' * 20)
+    print('Time')
+    # print(time_to_end)
+    print('-' * 20)
+    print('Flipped 0 -> 1')
 
 flip0_matrix = []
 flip0_sol_tot = 0
 
 c = 0
 while c < cells:
-	m = 0
-	row = []
-	while m < mutations:
-		if not isinstance(F0[c, m], int):
-			row.append(int(F0[c, m].X))
-			flip0_sol_tot += int(F0[c, m].X)
-		else:
-			row.append(0)
-		m += 1
-	if verbose:
-		print(' '.join([str(x) for x in row]))
-	flip0_matrix.append(row)
-	c += 1
+    m = 0
+    row = []
+    while m < mutations:
+        if not isinstance(F0[c, m], int):
+            row.append(int(F0[c, m].X))
+            flip0_sol_tot += int(F0[c, m].X)
+        else:
+            row.append(0)
+        m += 1
+    if verbose:
+        print(' '.join([str(x) for x in row]))
+    flip0_matrix.append(row)
+    c += 1
 
 flip0_matrix = np.array(flip0_matrix)
 
 if verbose:
-	print('-' * 20)
-	print('Flipped 1 -> 0')
+    print('-' * 20)
+    print('Flipped 1 -> 0')
 flip1_matrix = []
 flip1_sol_tot = 0
 c = 0
 while c < cells:
-	m = 0
-	row = []
-	while m < mutations:
-		if not isinstance(F1[c, m], int):
-			row.append(int(F1[c, m].X))
-			flip1_sol_tot += int(F1[c, m].X)
-		else:
-			row.append(0)
-		m += 1
-	if verbose:
-		print(' '.join([str(x) for x in row]))
-	flip1_matrix.append(row)
-	c += 1
+    m = 0
+    row = []
+    while m < mutations:
+        if not isinstance(F1[c, m], int):
+            row.append(int(F1[c, m].X))
+            flip1_sol_tot += int(F1[c, m].X)
+        else:
+            row.append(0)
+        m += 1
+    if verbose:
+        print(' '.join([str(x) for x in row]))
+    flip1_matrix.append(row)
+    c += 1
 
 flip1_matrix = np.array(flip1_matrix)
 
 try:
-	os.makedirs(args.outDir)
+    os.makedirs(args.outDir)
 except OSError as exc:
-	if exc.errno == errno.EEXIST and os.path.isdir(args.outDir):
-		pass
-	else:
-		raise
+    if exc.errno == errno.EEXIST and os.path.isdir(args.outDir):
+        pass
+    else:
+        raise
 
 filename = os.path.splitext(os.path.basename(args.file))[0]
 outfile = os.path.join(args.outDir, filename)
@@ -323,50 +336,53 @@ removed_mutation_indeces = []
 solution_mutation_names = []
 m = 0
 while m < mutations:
-	value = int(K[m].X)
-	removed_cols.append(value)
-	if value == 0:
-		solution_mutation_names.append(mutation_names[m])
-	else:
-		removed_mutation_names.append(mutation_names[m])
-		removed_mutation_indeces.append(str(m+1))
-	m += 1
+    value = int(K[m].X)
+    removed_cols.append(value)
+    if value == 0:
+        solution_mutation_names.append(mutation_names[m])
+    else:
+        removed_mutation_names.append(mutation_names[m])
+        removed_mutation_indeces.append(str(m + 1))
+    m += 1
+
+# print(removed_cols)
+print(removed_mutation_names)
 
 file_out.write('cellID/mutID\t')
 file_out.write('\t'.join(solution_mutation_names))
 file_out.write('\n')
 
 if verbose:
-	print('-' * 20)
-	print('Result')
+    print('-' * 20)
+    print('Result')
 sol_20_tot = 0
 sol_21_tot = 0
 sol_matrix = []
 c = 0
 while c < cells:
-	m = 0
-	row = []
-	while m < mutations:
-		if int(K[m].X) == 0:
-			if matrix_input[c, m] == 0:
-				row.append(int(F0[c, m].X))
-			elif matrix_input[c, m] == 1:
-				row.append(1 - int(F1[c, m].X))
-			else:
-				row.append(int(X[c, m].X))
-				if int(X[c, m].X) == 0:
-					sol_20_tot += 1
-				else:
-					sol_21_tot += 1
-		m += 1
-	if verbose:
-		print(' '.join([str(x) for x in row]))
-	file_out.write('%s\t' % cell_names[c])
-	file_out.write('\t'.join([str(x) for x in row]))
-	file_out.write('\n')
+    m = 0
+    row = []
+    while m < mutations:
+        if int(K[m].X) == 0:
+            if matrix_input[c, m] == 0:
+                row.append(int(F0[c, m].X))
+            elif matrix_input[c, m] == 1:
+                row.append(1 - int(F1[c, m].X))
+            else:
+                row.append(int(X[c, m].X))
+                if int(X[c, m].X) == 0:
+                    sol_20_tot += 1
+                else:
+                    sol_21_tot += 1
+        m += 1
+    if verbose:
+        print(' '.join([str(x) for x in row]))
+    file_out.write('%s\t' % cell_names[c])
+    file_out.write('\t'.join([str(x) for x in row]))
+    file_out.write('\n')
 
-	sol_matrix.append(row)
-	c += 1
+    sol_matrix.append(row)
+    c += 1
 
 sol_matrix = np.array(sol_matrix)
 
@@ -388,50 +404,49 @@ log.write('RUNNING_TIME_SECONDS: {0:.3f}\n'.format(time_to_run.total_seconds()))
 # --- DOUBLE-CHECK PP
 conflict_free = True
 for p in range(sol_matrix.shape[1]):
-	for q in range(p + 1, sol_matrix.shape[1]):
-		oneone = False
-		zeroone = False
-		onezero = False
-		for r in range(sol_matrix.shape[0]):
-			if sol_matrix[r][p] == 1 and sol_matrix[r][q] == 1:
-				oneone = True
-			if sol_matrix[r][p] == 0 and sol_matrix[r][q] == 1:
-				zeroone = True
-			if sol_matrix[r][p] == 1 and sol_matrix[r][q] == 0:
-				onezero = True
+    for q in range(p + 1, sol_matrix.shape[1]):
+        oneone = False
+        zeroone = False
+        onezero = False
+        for r in range(sol_matrix.shape[0]):
+            if sol_matrix[r][p] == 1 and sol_matrix[r][q] == 1:
+                oneone = True
+            if sol_matrix[r][p] == 0 and sol_matrix[r][q] == 1:
+                zeroone = True
+            if sol_matrix[r][p] == 1 and sol_matrix[r][q] == 0:
+                onezero = True
 
-		if oneone and zeroone and onezero:
-			conflict_free = False
-			if verbose:
-				print('Conflict in columns (%d, %d)' % (p, q))
+        if oneone and zeroone and onezero:
+            conflict_free = False
+            print('Conflict in columns (%d, %d)' % (p, q))
 
 if conflict_free:
-	conflict_free = 'YES'
+    conflict_free = 'YES'
 else:
-	conflict_free = 'NO'
+    conflict_free = 'NO'
 
 log.write('IS_CONFLICT_FREE: {0}\n'.format(conflict_free))
 
 log.write('TOTAL_FLIPS_REPORTED: {0}\n'.format(
-	str(flip0_sol_tot + flip1_sol_tot)))
+    str(flip0_sol_tot + flip1_sol_tot)))
 log.write('0_1_FLIPS_REPORTED: {0}\n'.format(
-	str(flip0_sol_tot)))
+    str(flip0_sol_tot)))
 log.write('1_0_FLIPS_REPORTED: {0}\n'.format(
-	str(flip1_sol_tot)))
+    str(flip1_sol_tot)))
 log.write('2_0_FLIPS_REPORTED: {0}\n'.format(
-	str(sol_20_tot)))
+    str(sol_20_tot)))
 log.write('2_1_FLIPS_REPORTED: {0}\n'.format(
-	str(sol_21_tot)))
+    str(sol_21_tot)))
 log.write('MUTATIONS_REMOVED_UPPER_BOUND: {0}\n'.format(
-	str(args.maxMut)))
+    str(args.maxMut)))
 log.write('MUTATIONS_REMOVED_NUM: {0}\n'. format(
-	str(sum(removed_cols))))
+    str(sum(removed_cols))))
 log.write('MUTATIONS_REMOVED_INDEX: {0}\n'.format(
-	','.join(removed_mutation_indeces)))
+    ','.join(removed_mutation_indeces)))
 
 log.close()
 
 if tree:
-	# Tree construction
-	from tree import *
-	write_tree(sol_matrix, mutation_names, outfile)
+    # Tree construction
+    from tree import *
+    write_tree(sol_matrix, mutation_names, outfile)
